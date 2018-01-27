20ab092a7e78db48b3280c6d048721bb64291c1d
Malinovskiy <malinovskiyst@gmail.com>
2018-01-27 21:07:46 +0300

    True?

--- task_01_04.py
+++ task_01_04.py
@@ -7,7 +7,7 @@ y2 = int(input())
 a = (x - x1)**2 + (y - y1)**2
 b = (x1 - x2)**2 + (y1 - y2)**2
 c = (x - x2)**2 + (y - y2)**2
-if bool((a + b)**2 == c**2) | bool((a + c)**2 == b**2) | bool((b + c)**2 == a**2) == True:
+if bool((a + b)**2 == c**2) | bool((a + c)**2 == b**2) | bool((b + c)**2 == a**2):
     print('yes')
 else:
     print('no')
