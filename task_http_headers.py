8c0996ea16a1e557b50f20af471b10ec77d130fb
Stanislav <stanislav@linux-havc.suse>
2018-03-05 02:47:07 +0300

    заголовки

--- task_http_headers.py
+++ task_http_headers.py
@@ -1,5 +1,33 @@
 import json
 
+# headers = 'headers-1.txt'
+# results = 'results.json'
 
-with open('header.txt') as f:
-    for i in f:
+
+def http_headers_to_json(headers, results):
+    with open(headers) as fuck:
+        dick = {}
+        for i in fuck:
+            line = i.strip().split(':', 1)
+            if 'HTTP' in i and 'H' == i[0]:
+                for j in line:
+                    j = j.split(' ')
+                    dick.setdefault('protocol', j[0])
+                    dick.setdefault('status_code', j[1])
+                    dick.setdefault('status_message', j[2])
+            elif 'HTTP' in i and 'H' != i[0]:
+                for j in line:
+                    j = j.split(' ')
+                    dick.setdefault('method', j[0])
+                    dick.setdefault('uri', j[1])
+                    dick.setdefault('protocol', j[2])
+            else:
+                if line[0] in dick.keys():
+                    dick[line[0]] = line[1]
+                else:
+                    dick.setdefault(line[0], line[1])
+    with open(results, 'w') as res:
+        json.dump(dick, res)
+    return
+
+# http_headers_to_json(headers,results)
